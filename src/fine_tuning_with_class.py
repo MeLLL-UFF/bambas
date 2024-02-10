ccf0087651e55ad7051991f7ecf8b3cf44a6f4be
Luiz Matos <lfmatosmelo@id.uff.br>
2024-02-10 18:28:31 -0300

    feat(ft_with_class): bernice changes

--- src/fine_tuning_with_class.py
+++ src/fine_tuning_with_class.py
@@ -14,18 +14,21 @@ from huggingface_hub import interpreter_login
 from src.subtask_1_2a import evaluate_h, hf1_score, hprec_score, hrec_score
 from src.subtask_1_2a import get_dag_labels
 from src.classification import save_predictions
+from src.confusion_matrix import binary_relevance_confusion_matrix
 from sklearn.preprocessing import MultiLabelBinarizer
 from transformers import AutoModel, AutoTokenizer, DataCollatorWithPadding
 from torch.utils.data import DataLoader
+from typing import Tuple, List
 
 OUTPUT_DIR = f"{get_workdir()}/fine_tuning_with_class"
-
+GOLD_PATH = f"{get_workdir()}/dataset/semeval2024/subtask1/validation.json"
 
 def fine_tune(args: Namespace):
     model = args.model
     dataset = args.dataset
     fine_tuned_name = args.fine_tuned_name
     batch_size = args.batch_size
+    max_length = args.max_length
     lr = args.lr
     weight_decay = args.weight_decay
     save_model = args.save_model
@@ -38,34 +41,23 @@ def fine_tune(args: Namespace):
     print("Merged dataset length:", len(train_df))
 
     labels = [get_dag_labels()]
+    labels[0].sort()
     mlb = MultiLabelBinarizer(classes=labels[0])
     train_labels = np.asarray(mlb.fit(labels).transform(train_df["labels"].to_numpy()), dtype=float).tolist()
     train_df["labels"] = train_labels
     dev_labels = np.asarray(mlb.transform(dev_df["labels"].to_numpy()), dtype=float).tolist()
     dev_df["labels"] = dev_labels
 
-    # id2label = {mlb.transform([[label]])[0][0]:label for label in labels[0]}
-    # print(id2label)
-    # label2id = {mlb.inverse_transform([[id]])[0][0]:id for id in id2label.keys()}
-    # print(label2id)
-
     train, dev = map(Dataset.from_pandas, [train_df, dev_df])
 
-    # def remove_additional_columns(ds: Dataset):
-    #     columns = ds.column_names
-    #     to_remove = [col for col in columns if col != "text" and col != "labels"]
-    #     return ds.remove_columns(to_remove)
-
-    # train, dev = map(remove_additional_columns, [train, dev])
-
-    tokenizer = AutoTokenizer.from_pretrained(model, use_fast=True)
+    tokenizer = AutoTokenizer.from_pretrained(model, use_fast=True, model_max_length=max_length)
     device_map = {"": "cpu"}
     if torch.cuda.is_available():
         device_map = {"": 0}
     print(f"Using device: {device_map}")
 
     def tokenize(data):
-        encoding = tokenizer(data["text"], padding=True, truncation=True)
+        encoding = tokenizer(data["text"], max_length=max_length, padding=True, truncation=True)
         encoding["labels"] = data["labels"]
         return encoding
 
@@ -107,9 +99,6 @@ def fine_tune(args: Namespace):
         return predictions
 
     def compute_metrics(p: EvalPrediction):
-        print("p: ", p)
-        print("p.predictions: ", p.predictions)
-        print("p.label_ids: ", p.label_ids)
         preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
         predictions = logits_to_predictions(preds)
 
@@ -149,52 +138,57 @@ def fine_tune(args: Namespace):
         print(f"Uploading fine-tuned model ({fine_tuned_name}) to HuggingFace Hub")
         trainer.push_to_hub()
     
+    def predict(ds: Dataset) -> Tuple[List[str], np.ndarray]:
+        # Create DataCollator object
+        data_collator = DataCollatorWithPadding(tokenizer=tokenizer, padding="max_length", max_length=max_length)
+        # Create DataLoader to be returned
+        dl = DataLoader(dataset=ds,
+                        batch_size=batch_size,
+                        collate_fn=data_collator)
+        test_predicted_labels = None
+        raw_predictions = None
+        with torch.no_grad():
+            for idx, batch in enumerate(dl):
+                print(f'Batch no. {idx+1}. Transferring to device')
+                encoding = {k:v.to(trainer.model.device) for k, v in batch.items()}
+                print("Prediction pass")
+                outputs = trainer.model(**encoding)
+                print("Retrieving logits")
+                logits = outputs.logits
 
-    # Prediction
-    print("Predicting for test file. Tokenizing test df")
-    ds = Dataset.from_pandas(test_df)
+                print("Parsing logits")
+                sigmoid = torch.nn.Sigmoid()
+                probs = sigmoid(logits.cpu())
+                predictions = np.zeros(probs.shape)
+                predictions[np.where(probs >= 0.5)] = 1
 
-    def tokenize_test(data):
-        encoding = tokenizer(data["text"], padding=True, truncation=True)
-        return encoding
+                print("Concatenating raw predictions")
+                if raw_predictions is None:
+                    raw_predictions = predictions
+                else:
+                    raw_predictions = np.concatenate((raw_predictions, predictions))
+                
+                print("Transforming to predictions")
+                pred = mlb.inverse_transform(predictions)
+                if test_predicted_labels is None:
+                    test_predicted_labels = pred
+                else:
+                    test_predicted_labels = np.concatenate((test_predicted_labels, pred))
+        return test_predicted_labels.tolist(), np.asarray(raw_predictions, dtype=int)
     
-    ds = ds.map(tokenize_test, batched=True, batch_size=batch_size, num_proc=4, remove_columns=ds.column_names)
-    # Remove original text from dataset
+    # # We manually evaluate so we can compute the confusion matrix
+    # print("Manual prediction on dev_set")
+    # dev_predicted_labels, dev_raw_predictions = predict(dev)
 
-    # Create DataCollator object
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, padding="max_length")
-    # Create DataLoader to be returned
-    dl = DataLoader(dataset=ds,
-                    batch_size=batch_size,
-                    collate_fn=data_collator)
-    test_predicted_labels = None
-    with torch.no_grad():
-        for idx, batch in enumerate(dl):
-            print(f'Batch no. {idx+1}. Transferring to device')
-            encoding = {k:v.to(trainer.model.device) for k, v in batch.items()}
-            print("Prediction pass")
-            outputs = trainer.model(**encoding)
-            print("Retrieving logits")
-            logits = outputs.logits
+    # ts = int(time.time())
+    # pred_path, _ = save_predictions(dev_df, dev_predicted_labels, "dev", ts)
 
-            print("Parsing logits")
-            sigmoid = torch.nn.Sigmoid()
-            probs = sigmoid(logits.cpu())
-            predictions = np.zeros(probs.shape)
-            predictions[np.where(probs >= 0.5)] = 1
-            
-            print("Transforming to predictions")
-            pred = mlb.inverse_transform(predictions)
-            if test_predicted_labels is None:
-                test_predicted_labels = pred
-            else:
-                test_predicted_labels = np.concatenate((test_predicted_labels, pred))
+    # prec, rec, f1 = evaluate_h(pred_path, GOLD_PATH)
+    # print(f"\nValidation set:\n\tPrecision: {prec}\n\tRecall: {rec}\n\tF1: {f1}\n")
 
-    ts = int(time.time())
-    pred_path, _ = save_predictions(test_df, test_predicted_labels, "dev_unlabeled", ts)
-    print(f"Finished successfully. dev_unlabeled predictions saved at {pred_path}")
+    # cf_mtx = binary_relevance_confusion_matrix(np.asarray(dev_labels, dtype=int), dev_raw_predictions, labels[0])
 
-    # results_csv_path = f"{OUTPUT_DIR}/results.csv"
+    # results_csv_path = f"{OUTPUT_DIR}/ft_with_class_results.csv"
     # print(f"Saving validation set results to {results_csv_path}")
     # if os.path.exists(results_csv_path):
     #     results = pd.read_csv(results_csv_path, index_col=0)
@@ -204,36 +198,70 @@ def fine_tune(args: Namespace):
     #     os.makedirs(dir, exist_ok=True)
     #     results = pd.DataFrame(
     #         columns=[
-    #             "FE Model",
-    #             "FE Method",
-    #             "FE Layers",
-    #             "FE Layers Agg Method",
-    #             "FE Dataset",
+    #             "Model",
+    #             "Dataset",
     #             "Test Dataset",
-    #             "Classifier",
     #             "F1",
     #             "Precision",
     #             "Recall",
+    #             "Confusion Matrix"
     #             "Timestamp"])
     
-    # results.loc[len(results.index) + 1] = [train_ft_info["model"],
-    #                                        train_ft_info["extraction_method"],
-    #                                        train_ft_info["layers"],
-    #                                        train_ft_info["layers_aggregation_method"],
-    #                                        train_ft_info["dataset"],
+    # results.loc[len(results.index) + 1] = [args.model,
+    #                                        args.dataset,
     #                                        "dev_set",
-    #                                        args.classifier,
     #                                        f1,
     #                                        prec,
     #                                        rec,
+    #                                        cf_mtx,
     #                                        ts]
     # results.to_csv(results_csv_path)
+    
+    # # Prediction
+    # print("Predicting for test file. Tokenizing test df")
+    # ds = Dataset.from_pandas(test_df)
+
+    # def tokenize_test(data):
+    #     encoding = tokenizer(data["text"], padding=True, truncation=True)
+    #     return encoding
+    
+    # ds = ds.map(tokenize_test, batched=True, batch_size=batch_size, num_proc=4, remove_columns=ds.column_names)
+    # # Remove original text from dataset
+
+    # # Create DataCollator object
+    # data_collator = DataCollatorWithPadding(tokenizer=tokenizer, padding="max_length")
+    # # Create DataLoader to be returned
+    # dl = DataLoader(dataset=ds,
+    #                 batch_size=batch_size,
+    #                 collate_fn=data_collator)
+    # test_predicted_labels = None
+    # with torch.no_grad():
+    #     for idx, batch in enumerate(dl):
+    #         print(f'Batch no. {idx+1}. Transferring to device')
+    #         encoding = {k:v.to(trainer.model.device) for k, v in batch.items()}
+    #         print("Prediction pass")
+    #         outputs = trainer.model(**encoding)
+    #         print("Retrieving logits")
+    #         logits = outputs.logits
+
+    #         print("Parsing logits")
+    #         sigmoid = torch.nn.Sigmoid()
+    #         probs = sigmoid(logits.cpu())
+    #         predictions = np.zeros(probs.shape)
+    #         predictions[np.where(probs >= 0.5)] = 1
+            
+    #         print("Transforming to predictions")
+    #         pred = mlb.inverse_transform(predictions)
+    #         if test_predicted_labels is None:
+    #             test_predicted_labels = pred
+    #         else:
+    #             test_predicted_labels = np.concatenate((test_predicted_labels, pred))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("fine_tuning",
                                      description="fine-tuning with masked-language model (MLM) objective for language models")
-    parser.add_argument("--model", type=str, choices=["xlm-roberta-base"], help="model to fine-tune", required=True)
+    parser.add_argument("--model", type=str, choices=["xlm-roberta-base", "jhu-clsp/bernice"], help="model to fine-tune", required=True)
     parser.add_argument(
         "--dataset",
         type=str,
@@ -242,7 +270,8 @@ if __name__ == "__main__":
         required=True)
     parser.add_argument("--fine_tuned_name", type=str, help="fine-tuned model name", required=True)
     parser.add_argument("--batch_size", type=int, help="batch size for pretraining", default=32)
-    parser.add_argument("--lr", type=float, default=2e-4, help="training learning rate")
+    parser.add_argument("--max_length", type=int, help="max sentence length for truncation", default=128)
+    parser.add_argument("--lr", type=float, default=3.9e-5, help="training learning rate")
     parser.add_argument("--weight_decay", type=float, default=0.001, help="training weight decay")
     parser.add_argument("--save_model", action="store_true", help="wheter to save adjusted model locally")
     parser.add_argument(
