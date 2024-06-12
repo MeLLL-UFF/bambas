7dcda9af2cf867ffc0e04776ed377caf647cabf8
Allrthur <athurbittencourt@id.uff.br>
2024-06-12 16:02:33 -0300

    creating a 2D scatterplot for paraphrase visualization

--- src/paraphrase_dist.py
+++ src/paraphrase_dist.py
@@ -5,6 +5,7 @@ from typing import List, Dict, Any, Tuple
 from imblearn.over_sampling import SMOTE
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 from sklearn.metrics.pairwise import cosine_similarity
 
 ORIGINAL_FTS_PATH = "/home/arthur/Documents/Trab/NLP/bambas/feature_extraction_paraphrasing/paraphrasing_4f/test_features_array.json"
@@ -26,27 +27,48 @@ if __name__ == "__main__":
     original_fts = load_features_array(ORIGINAL_FTS_PATH)
     paraphrase_fts = load_features_array(PARAPHRASE_FTS_PATH)
 
+    smote = SMOTE(sampling_strategy={0:len(original_fts)*2, 1:len(original_fts)})
+    dummy_labels = [0]*len(original_fts)
+    dummy_labels.extend([1]*len(original_fts))
+    smote_fts,smote_lbls = smote.fit_resample(np.concatenate([original_fts, original_fts]), dummy_labels)
+    smote_fts = smote_fts[:len(original_fts)]
 
+    # Reduce dimensionality of features
+    pca = PCA(copy=True, n_components=2)
+    original_reduced = pd.DataFrame(pca.fit_transform(X=original_fts), columns=["X", "Y"])
+    paraphrase_reduced = pd.DataFrame(pca.fit_transform(X=paraphrase_fts), columns=["X", "Y"])
+    smote_reduced = pd.DataFrame(pca.fit_transform(X=smote_fts), columns=["X", "Y"])
+    
+    original_reduced["src"] = ["original"]*len(original_reduced)
+    paraphrase_reduced["src"] = ["paraphrase"]*len(paraphrase_reduced)
+    smote_reduced["src"] = ["smote"]*len(smote_reduced)
+
+    data = pd.concat([
+        original_reduced,
+        paraphrase_reduced, 
+        smote_reduced,
+    ]) 
+
+    # Create Scatterplot
+    scatterplot = sns.scatterplot(data=data, x="X", y="Y", hue="src")
+    import matplotlib.pyplot as plt
+    plt.show()
+    exit()
+
+    # Calculate Distances
     # distances = [euclidian_dist(original, paraphrase) for original,paraphrase in zip(original_fts, paraphrase_fts)]
-    distances = cosine_similarity(original_fts, paraphrase_fts)
+    # distances = cosine_similarity(original_fts, paraphrase_fts)
     # print("Distances between original and paraphrases")
     # print(distances)
-
-    # smote = SMOTE(sampling_strategy={0:6, 1:12})
-    # smote_fts,_ = smote.fit_resample(np.concatenate([original_fts, original_fts]), np.array([0,0,0,0,0,0,1,1,1,1,1,1]))
-    # synthetic_smote_fts = smote_fts[-6:]
-    # distances = [euclidian_dist(original, paraphrase) for original,paraphrase in zip(original_fts, synthetic_smote_fts)]
-    # print("Distances between original and smote")
-    # print(distances)
     
-    distance_matrix =  []
-    fts_concat = np.concatenate([original_fts, paraphrase_fts])
+    # distance_matrix =  []
+    # fts_concat = np.concatenate([original_fts, paraphrase_fts])
     
-    for home_ft in fts_concat:
-        matrix_line = []
-        for out_ft in fts_concat:
-            matrix_line.append(euclidian_dist(home_ft, out_ft))
-        distance_matrix.append(matrix_line)
+    # for home_ft in fts_concat:
+    #     matrix_line = []
+    #     for out_ft in fts_concat:
+    #         matrix_line.append(euclidian_dist(home_ft, out_ft))
+    #     distance_matrix.append(matrix_line)
     
     # print(distance_matrix)
 
